3c8b3cd392bbc6c54b44436cd808e1a2ccd4f237
Otto Sipe <ottosipe@gmail.com>
2013-01-28 21:53:14 -0500

    hotfix for deploy

--- views.py
+++ views.py
@@ -12,12 +12,19 @@ import httplib2
 from oauth2client.appengine import OAuth2Decorator
 from apiclient.discovery import build
 
+"""
 decorator = OAuth2Decorator(
   client_id='100813449163-vl4u1p376hjrc1e3gc090e8miarjnir3.apps.googleusercontent.com',
   client_secret='L3TfdZSaobeM9EANnUhivnyg',
   scope='https://www.googleapis.com/auth/calendar',
   user_agent='myTime')
-
+"""
+decorator = OAuth2Decorator(
+  client_id='100813449163-26s1qdti9fukt0up65lljpg24oijl33d.apps.googleusercontent.com',
+  client_secret='E6xiDmvf_cB6RF8WaqYj13Hf',
+  scope='https://www.googleapis.com/auth/calendar',
+  user_agent='myTime')
+  
 service = build('calendar', 'v3')
 
 class MainPage(jade.jadeHandler):
