eaed5c9b0f46680259507c1ddd7a57c54cef2145
Josh Billingham <wjbillin@umich.edu>
2013-02-13 02:19:59 -0500

    dumping

--- api.py
+++ api.py
@@ -188,7 +188,7 @@ class Reminders(webapp2.RequestHandler):
     student.reminders += newReminder
     student.put()
 
-    self.response.out.write(json.dumps(newReminder))
+    self.response.out.write(json.dumps(newReminder.to_dict()))
   
   def get(self):
     User = users.get_current_user()
